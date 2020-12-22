8a4c45ad6201689ea1b7e924000ffeffbb5be228
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-12-21 20:22:01 -0800

    commit tree a6acaf5802f26740574487ed8378a0dceafeb0ed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, callback){
+    let result = [];
+    for (let element of array) {
+        result.push(callback(element));
+    }
+    return result;
+};
+
+function reduce(array, callback, start){
+    let result = true;
+    for (let element of array) {
+        result = callback(element, result)
+        console.log(element, result)
+    }
+    if (result !== true && result !== false) {
+        console.log(`error, ${result}`)
+       result = result - 1
+    }
+    if (start) {
+        result += start;
+        console.log(`error, ${result}`)
+    }
+    return result;
+}
